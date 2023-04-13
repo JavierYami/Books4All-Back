889625290e4ee676b061e1ca0ef643cefd75076d
JavierYami <javieyami1@outlook.com>
2023-04-13 13:58:35 -0600

    Se agregó controller por query

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { sequelize } = require('./src/DB_connection');
 
 sequelize.sync({ force: false }).then(async () => {
   console.log('connected database, master');
-  // await getBooks();
-  server.listen(3001, () => {
+  // const books =await getBooks();
+  server.listen(3001, () => { 
     console.log("listening on port 3001 - testing changes");
   });
 });
