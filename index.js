34fee35ca6831ba13666f73b37380cace2a3f2d5
Roberto Ochoa <oc_005@hotmail.com>
2023-04-13 16:54:25 -0300

    controller:getOneBook

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const server = require("./src/app");
 const { getBooks } = require("./src/controllers/SaveApiData");
-const { sequelize } = require('./src/DB_connection');
-
-
+const { sequelize } = require("./src/DB_connection");
 
 sequelize.sync({ force: false }).then(async () => {
-  console.log('connected database, master');
-  // await getBooks();
+  console.log("connected database, master");
+  /*   await getBooks(); */
   server.listen(3001, () => {
     console.log("listening on port 3001 - testing changes");
   });
