c2c43234b71376cd27f7c55d0aea31452ded8f96
JavierYami <javieyami1@outlook.com>
2023-04-14 07:18:00 -0600

    ruta review por id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { sequelize } = require("./src/DB_connection");
 
 sequelize.sync({ force: false }).then(async () => {
   console.log('connected database, master');
-  getBooks();
+  // getBooks();
   server.listen(3001, () => {
   // const books =await getBooks();
     console.log("listening on port 3001 - testing changes");
